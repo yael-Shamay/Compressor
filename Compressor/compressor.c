ccd4f705f8e58bb10598f7666d7279da9a507dec
pc <pc@1>
2022-07-20 23:51:15 +0300

    Add project files.

---
+++ Compressor/compressor.c
@@ -0,0 +1,7 @@
+#include "deCompression.h"
+#include "compression.h"
+#include <stdio.h>
+#include<stdlib.h>
+void main() {
+	printf("hellow to project");
+}
---
+++ Compressor/deCompression.c
@@ -0,0 +1,1 @@
+#include "deCompression.h"
